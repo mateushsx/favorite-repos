c4e9fcaf9e14ab972d06e8ad39f21162569fdb41
Mateus Henrique <mateusdzn10@gmail.com>
2024-06-11 21:58:57 -0300

    feat: add structure and styles of main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,3 +1,26 @@
+import { Github, Plus } from 'lucide-react';
+
 export function Main() {
-  return <div>Main</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col gap-6 bg-slate-200 p-4 max-w-md w-full rounded mx-4 pb-8">
+        <header className="flex gap-2">
+          <Github size={32} className="text-slate-900" />
+          <h1 className="text-2xl text-slate-900 font-bold">Repositórios</h1>
+        </header>
+
+        <div className="flex gap-1 w-full">
+          <input
+            type="text"
+            placeholder="Repositório"
+            className="p-1 outline-none text-black border border-slate-300 w-full"
+          />
+
+          <button type="submit" className="bg-green-700 p-1 rounded">
+            <Plus />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
